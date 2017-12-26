69d0a3e85ebe991df5e84b5f6b23e74b5ab22ce4
474875346 <474875346@qq.com>
2017-12-26 11:29:36 +0800

    通知详情

--- EgoviOS-Swift/Base/BaseModel.swift
+++ EgoviOS-Swift/Base/BaseModel.swift
@@ -11,14 +11,14 @@ import UIKit
 class BaseModel: NSObject {
     //MARK:成功弹框
     func SuccessTost(Title:String , Body:String) -> Void {
-        SwiftMessageManager.showMessage(layoutType: .MessageView, themeType: .Success, iconImageType: .light, presentationStyleType: .top, title: Title, body: Body, isHiddenBtn: true, seconds: 1)
+        SwiftMessageManager.showMessage(layoutType: .CardView, themeType: .Success, iconImageType: .light, presentationStyleType: .top, title: Title, body: Body, isHiddenBtn: true, seconds: 1)
     }
     //MARK:警告弹框
     func WaringTost(Title:String , Body:String) -> Void {
-        SwiftMessageManager.showMessage(layoutType: .MessageView, themeType:.Warning, iconImageType:.light, presentationStyleType:.top, title: Title, body: Body, isHiddenBtn: true, seconds: 1)
+        SwiftMessageManager.showMessage(layoutType: .CardView, themeType:.Warning, iconImageType:.light, presentationStyleType:.top, title: Title, body: Body, isHiddenBtn: true, seconds: 1)
     }
     //MARK:错误弹框
     func ErrorTost() -> Void {
-        SwiftMessageManager.showMessage(layoutType: .MessageView, themeType: .Error, iconImageType: .light, presentationStyleType: .top, title: "", body: "网络问题，请休息一下", isHiddenBtn: true, seconds: 1)
+        SwiftMessageManager.showMessage(layoutType: .CardView, themeType: .Error, iconImageType: .light, presentationStyleType: .top, title: "", body: "网络问题，请休息一下", isHiddenBtn: true, seconds: 1)
     }
 }
